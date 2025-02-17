d37094b45ffa1abb71abf006fe9699442bb8e7fd
Mohamed Meeran <148966222+mdmeeran90@users.noreply.github.com>
2025-02-18 00:50:59 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,41 +1,30 @@
-const ProductModel = require('../models/productModel')
+const ProductModel = require('../models/productModel');
 
-// Get Products API - /api/v1/products
+//Get Products API - /api/v1/products
 exports.getProducts = async (req, res, next) => {
-    const query = req.query.keyword ? {
-        name: {
-            $regex: req.query.keyword,
-            $options: 'i'
-        }
-    } : {}
+    const query = req.query.keyword?{ name : { 
+        $regex: req.query.keyword,
+        $options: 'i'
+     }}:{}
     const products = await ProductModel.find(query);
-
-    res.json(
-        {
-            success: true,
-            products
-        }
-    )
+    res.json({
+        success: true,
+        products
+    })
 }
 
-// Get Single Product API - /api/v1/product/:id(6749a30a98144581367429c4)
+//Get Single Product API - /api/v1/product/:id
 exports.getSingleProduct = async (req, res, next) => {
     try {
-        const product = await ProductModel.findById(req.params.id)
-
-        res.json(
-            {
-                success: true,
-                product
-            }
-        )
-    }
-    catch (error) {
-        res.status(404).json(
-            {
-                success: false,
-                message: 'Unable to get product with that ID'
-            }
-        )
+        const product = await ProductModel.findById(req.params.id);
+        res.json({
+            success: true,
+            product
+        })
+    } catch (error) {
+        res.status(404).json({
+            success: false,
+            message: 'Unable to get Product with that ID'
+        })
     }
 }
